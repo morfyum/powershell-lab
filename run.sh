7b15063d08c60770c6860acf4be8bcc51fa3bf6b
Morfyum <morfyum@gmail.com>
2023-04-05 21:04:20 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #podman run --rm -d --cpus="2" --memory 64M --pids-limit="128" --name mbot.py 000-mbot.py:0.2
 
-podman run --rm -it --volume powershell:/usr/src/app/mount --name pwsh-dev powershell-dev-env:0.2
+#podman run --rm -it --volume powershell:/usr/src/app/ --name pwsh-dev powershell-dev-env:0.2
+
+podman run --rm -it --privileged=true --volume ./src/:/usr/src/app/src --name pwsh-dev powershell-dev-env:0.2
+
