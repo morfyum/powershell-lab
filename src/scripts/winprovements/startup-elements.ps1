7b36199a919718a1b0447cb04da091e077fe743f
morfyum <morfyum@gmail.com>
2023-05-01 21:24:23 +0200

    cleanup: add checkResult and handleUserInput functions instead of hardcode

--- src/scripts/winprovements/startup-elements.ps1
+++ src/scripts/winprovements/startup-elements.ps1
@@ -4,29 +4,44 @@ $startup_elements = (Get-CimInstance Win32_StartupCommand | Select-Object Name, 
 $startup_elements_location = (Get-CimInstance Win32_StartupCommand | Select-Object Name, Location).Location
 $startup_counter = 0
 
-foreach ($element in $startup_elements) {
-    Write-Host "*** [" $startup_elements[$startup_counter], "] on [", $startup_elements_location[$startup_counter],"] ***" -ForegroundColor Green
 
-    $user_input = Read-Host "  - Do you want to Disable? [y/N]" -MaskInput
-    $user_input.ToLower() | Out-Null
-    if ($user_input -eq "y" ) {
-        #pass
-    } else {
-        #pass
-    }
-
-    # Startup Exceptions: Security Health
-    if ($startup_elements[$startup_counter] -eq "SecurityHealth") {
-        Write-Host "  - Skip Startup Exception:" $startup_elements[$startup_counter] -ForegroundColor Yellow
-    } else {
-        reg add $startup_elements_location[$startup_counter] /v $startup_elements[$startup_counter] /t REG_DWORD /d 0 /f | Out-Null
-    }
-
+function checkResult() {
     if ( $? -eq $true) {
         Write-Host "  - Success" -ForegroundColor Green
     } elseif ($? -eq $false) {
         Write-Host "  - Failed" -ForegroundColor Red
         exit 1
+    }
+}
+
+function handleUserInput() {
+    $user_input = Read-Host "  - Do you want to Disable? [y/N]"
+    $user_input.ToLower() | Out-Null
+    if ($user_input -eq "y" ) {
+        reg add $startup_elements_location[$startup_counter] /v $startup_elements[$startup_counter] /t REG_DWORD /d 0 /f | Out-Null
+        checkResult
+    } else {
+        Write-Host "  - Skip"
+    }
+}
+
+
+foreach ($element in $startup_elements) {
+    Write-Host "*** [" $startup_elements[$startup_counter], "] on [", $startup_elements_location[$startup_counter],"] ***" -ForegroundColor Green
+
+    # Startup Exceptions: Security Health
+    if ($startup_elements[$startup_counter] -eq "SecurityHealth") {
+        Write-Host "  - Skip Startup Exception:" $startup_elements[$startup_counter] -ForegroundColor Yellow
+    } else {
+        handleUserInput
+        #$user_input = Read-Host "  - Do you want to Disable? [y/N]"
+        #$user_input.ToLower() | Out-Null
+        <#if ($user_input -eq "y" ) {
+            reg add $startup_elements_location[$startup_counter] /v $startup_elements[$startup_counter] /t REG_DWORD /d 0 /f | Out-Null
+            checkResult
+        } else {
+            Write-Host "  - Skip"
+        }#>
     }
 
     $startup_counter = $startup_counter + 1
