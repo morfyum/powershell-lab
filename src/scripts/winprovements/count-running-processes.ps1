6a3afaef6a1ff63aeeb96bd209af415b38a59c80
morfyum <morfyum@gmail.com>
2023-05-01 21:11:27 +0200

    Visual Chages

--- src/scripts/winprovements/count-running-processes.ps1
+++ src/scripts/winprovements/count-running-processes.ps1
@@ -5,17 +5,16 @@
 # Get-Service | Where-Object {$_.Status -eq "Running"} | Measure-Object | Select-Object).Count
 #
 
-Write-Host "*** Count Processes: ( 76-80 before) ***" -ForegroundColor Yellow
+Write-Host "*** Running Processes ***" -ForegroundColor Cyan
+Write-Host "  - Default: 76-80 on Windows 10" -ForegroundColor Gray
 $running_processes = (Get-Process | Measure-Object | Select-Object).Count
-Write-Host "Processes: [ $running_processes ]"
+Write-Host "  - Currently running Processes: [ $running_processes ]" -ForegroundColor Green
 
-
-Write-Host "*** Count Services: ALL: 258 ***" -ForegroundColor Yellow
 $all_service = (Get-Service | Measure-Object | Select-Object).Count
-Write-Host "All: [ $all_service ]"
+#Write-Host "All: [ $all_service ]"
 
-
-Write-Host "*** Running Services: 78-80 ***" -ForegroundColor Yellow
+Write-Host "*** Running Services ***" -ForegroundColor Cyan
+Write-Host "  - Default: 78-80 on Windows 10 (all: $all_service)" -ForegroundColor Gray
 $running_services = (Get-Service | Where-Object {$_.Status -eq "Running"} | Measure-Object | Select-Object).Count
-Write-Host "Running [ $running_services ]"
+Write-Host "  - Currently Running: [ $running_services ]" -ForegroundColor Green
 
