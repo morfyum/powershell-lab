6e8bcd2badd2f96652789933164c539b08f44376
morfyum <morfyum@gmail.com>
2023-05-24 20:37:27 +0200

    NW: wmi-object documentation

---
+++ docs/wmi-objects.ps1
@@ -0,0 +1,19 @@
+﻿
+# WINDOWS LICENSE KEY (DPK) 
+## RunAs Admin. IfEmpty -> no Product Key!
+(Get-WmiObject -query ‘select * from SoftwareLicensingService’).OA3xOriginalProductKey
+
+#
+# Get-WmiObject DOCS:
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-wmiobject?view=powershell-5.1
+#
+# List of Win32_Objects:
+# https://learn.microsoft.com/en-us/windows/win32/cimwin32prov/win32-motherboarddevice
+
+
+# BIOS INFORMATIONS:
+Get-WmiObject -Class Win32_Bios | Format-List -Property *
+Get-WmiObject -Class Win32_Bios
+(Get-WmiObject -Class Win32_Bios).SMBIOSBIOSVersion
+
+
