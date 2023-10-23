6388d9a9ca3b9004dca87a674ff17a6e47fb384c
Morfyum <morfyum@gmail.com>
2023-10-23 12:53:21 +0100

    update

--- src/windows-only/wpf-in-ps/experimental-shell.ps1
+++ src/windows-only/wpf-in-ps/experimental-shell.ps1
@@ -3,18 +3,18 @@ $lastCommand = ""
 function Show-Menu {
 Clear-Host
 Write-Host "┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐"
-Write-Host "│ Q  : Quit | $lastCommand" -ForegroundColor DarkGreen
+Write-Host "  Q  : Quit | $lastCommand" -ForegroundColor DarkGreen
 Write-Host "└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘"
-Write-Host "| 0  : Option 0                | 10  : Windows 10 Activator"
-Write-Host "| 1  : WPF Applicatons         | 11  : Windows 11 Activator "
-Write-Host "| 2  : App manager             | 12  : - "
-Write-Host "| 3  : Option 4                | 13  : - "
-Write-Host "| 4  : Option 5                | 14  : - "
-Write-Host "| 5  : Option 6                | 15  : - "
-Write-Host "| 6  : Option 7                | 16  : - "
-Write-Host "| 7  : Option 8                | 17  : - "
-Write-Host "| 8  : Option 9                | 18  : - "
-Write-Host "| 9  : Option 10               | 19  : - "
+Write-Host "| 0  : Option 0                | 10  : Windows 10 Activator      | 20  : "
+Write-Host "| 1  : WPF Applicatons         | 11  : Windows 11 Activator      | 21  : "
+Write-Host "| 2  : App manager             | 12  : -                         | 22  : "
+Write-Host "| 3  : Option 4                | 13  : -                         | 23  : "
+Write-Host "| 4  : Option 5                | 14  : -                         | 24  : "
+Write-Host "| 5  : Option 6                | 15  : -                         | 25  : "
+Write-Host "| 6  : Option 7                | 16  : -                         | 26  : "
+Write-Host "| 7  : Option 8                | 17  : -                         | 27  : "
+Write-Host "| 8  : Option 9                | 18  : -                         | 28  : "
+Write-Host "| 9  : Option 10               | 19  : -                         | 29  : "
 Write-Host "-------------------------------------------------------------------------------------------------------------------"
 }
 
@@ -28,7 +28,8 @@ do {
     }
     $lastCommand = $shellInput
     switch ($shellInput) {
-        1  {$lastCommand = $lastCommand+': WPF App '; .\wpf-app.ps1} 
+        0  {$lastCommand = $lastCommand+': Try an another button '}
+        1  {$lastCommand = $lastCommand+': WPF App '; .\app.ps1} 
         2  {$lastCommand = $lastCommand+': You chose option #2'}
         3  {$lastCommand = $lastCommand+': You chose option #3'}
         10 {$lastCommand = $lastCommand+': W10 Activator '}
