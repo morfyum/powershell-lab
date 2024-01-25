55ddcfb8092e7f6b6e89f5bb373b697fd2a77301
Morfyum <morfyum@gmail.com>
2024-01-25 21:42:42 +0000

    rename Create-Menu to New-Menu for folliwing powershell standards

--- src/components/probe-Create-Menu.ps1
+++ src/components/probe-Create-Menu.ps1
@@ -2,5 +2,5 @@
 . .\Create-Menu
 
 # -Columns 2
-Create-Menu -MenuTitle "*** Powershell Navigation menu Optimization ***" -MenuOptions "Option 1 dasdsadadasd ada" , "Option 2 asdasdasd asdasdasd", "Option 3", "Option 4", "Option 5", "Option 6 Powershell Navigation menu Optimization" , "Option 7", "Option 8 asdasdasdasdasdasd", "Option 9 asdasdasdasdasd", "Option 10 asdasdasdasdasd+", "Option 11 dasdsadadasd ada" , "Option 12asdasdasdasdasd++", "Option 13asdasdasdasdasd+++", "Option 14", "Option 15", "Option 16 dasdsadadasd ada" , "Option 17", "Option 18", "Option 19", "Option 20 +++ +++ +++ +++ +++ +++ +++ ++++ ++++ +++ +++ ++ +++++ +++++ +++++ ++++++ ++", "Oooooption 21 :D ++++++++++++"
+New-Menu -MenuTitle "*** Powershell Navigation menu Optimization ***" -MenuOptions "Option 1 dasdsadadasd ada" , "Option 2 asdasdasd asdasdasd", "Option 3", "Option 4", "Option 5", "Option 6 Powershell Navigation menu Optimization" , "Option 7", "Option 8 asdasdasdasdasdasd", "Option 9 asdasdasdasdasd", "Option 10 asdasdasdasdasd+", "Option 11 dasdsadadasd ada" , "Option 12asdasdasdasdasd++", "Option 13asdasdasdasdasd+++", "Option 14", "Option 15", "Option 16 dasdsadadasd ada" , "Option 17", "Option 18", "Option 19", "Option 20 +++ +++ +++ +++ +++ +++ +++ ++++ ++++ +++ +++ ++ +++++ +++++ +++++ ++++++ ++", "Oooooption 21 :D ++++++++++++"
 
