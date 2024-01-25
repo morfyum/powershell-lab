30ab926c1ed272ede3c26aad1be8ddb867598bed
Morfyum <morfyum@gmail.com>
2024-01-25 21:47:05 +0000

    update

--- src/LAB/windows-only/wpf-in-ps/experimental-shell.ps1
+++ src/LAB/windows-only/wpf-in-ps/experimental-shell.ps1
@@ -1,10 +1,11 @@
-﻿
+﻿$host.UI.RawUI.WindowTitle = "MORFYUM"
+
 $lastCommand = ""
 function Show-Menu {
 Clear-Host
-Write-Host "┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐"
+Write-Host "┌──────────────────────────────────────────────────────────────────────────────────────────────────┐"
 Write-Host "  Q  : Quit | $lastCommand" -ForegroundColor DarkGreen
-Write-Host "└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘"
+Write-Host "└──────────────────────────────────────────────────────────────────────────────────────────────────┘"
 Write-Host "| 0  : Option 0                | 10  : Windows 10 Activator      | 20  :                           |"
 Write-Host "| 1  : WPF Applicatons         | 11  : Windows 11 Activator      | 21  :                           |"
 Write-Host "| 2  : App manager             | 12  : -                         | 22  :                           |"
@@ -15,12 +16,9 @@ Write-Host "| 6  : Set: RemoteSigned       | 16  : -                         | 2
 Write-Host "| 7  : Option 8                | 17  : -                         | 27  :                           |"
 Write-Host "| 8  : Option 9                | 18  : -                         | 28  :                           |"
 Write-Host "| 9  : Option 10               | 19  : -                         | 29  :                           |"
-Write-Host "-------------------------------------------------------------------------------------------------------------------"
+Write-Host "----------------------------------------------------------------------------------------------------"
 }
 
-function ShowExecutionPolicy {
-    return Get-ExecutionPolicy
-}
 
 function SetCurrentUserExecutionPolicy {
     param (
@@ -28,10 +26,10 @@ function SetCurrentUserExecutionPolicy {
         [string] $ExecutionPolicy
     )
     Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy $ExecutionPolicy
-    return ShowExecutionPolicy
+    return Get-ExecutionPolicy
 }
 
-ShowExecutionPolicy
+Get-ExecutionPolicy
 
 do {
     Show-Menu
@@ -47,7 +45,7 @@ do {
         1  {$lastCommand = $lastCommand+': WPF App '; ./app.ps1}
         2  {$lastCommand = $lastCommand+": You chose option #2"}
         3  {$lastCommand = $lastCommand+': You chose option #3'}
-        4  {$lastCommand = $lastCommand+": Policy status: [$(ShowExecutionPolicy)]"}
+        4  {$lastCommand = $lastCommand+": Policy status: [$(Get-ExecutionPolicy)]"}
         5  {$lastCommand = $lastCommand+": Set Restricted: [$(SetCurrentUserExecutionPolicy -ExecutionPolicy Restricted)]"}
         6  {$lastCommand = $lastCommand+": Set RemoteSigned: [$(SetCurrentUserExecutionPolicy -ExecutionPolicy RemoteSigned)]"}
         10 {$lastCommand = $lastCommand+': W10 Activator '}
