0e423067471a7454271cdc237345fd861a71a04e
Morfyum <morfyum@gmail.com>
2024-01-27 23:48:17 +0000

    NEW features added to shell.ps1
     - Show-Menu with generateRow() from UI.ps1
    
    New functions
    
     - isWTSession
    
     - getGPUs
    
    getSerialNumber
    
     - getwifiPercentage
    
     - getBatteryPercentage
    
    + small improvements

--- src/LAB/morfyum-toolkit/src/shell.ps1
+++ src/LAB/morfyum-toolkit/src/shell.ps1
@@ -1,11 +1,81 @@
-﻿# SELF CONFIGURATION AND VARIABLES 
+﻿# IMPORT MODULES / COMPONENTS
+. .\UI.ps1
+. .\core-utils.ps1
+
+# SELF CONFIGURATION AND VARIABLES 
 $selfLocation = (Get-Location).path
 $selfConfig = Get-Content -Path "$selfLocation\shell-config.json" | Out-String | ConvertFrom-Json
+$selfWidth = $host.UI.RawUI.WindowSize.Width
+$selfHeight = $host.UI.RawUI.WindowSize.Height
 $host.UI.RawUI.WindowTitle = "$($selfConfig.appName)"
+$host.UI.RawUI.foregroundColor = $($selfConfig.fontColor)
 $currentDate = Get-Date -Format "yyyy-MM-dd"
 $lastCommand = ""
 
+
+# EXTERNAL
+$biosVersion = (Get-WmiObject win32_bios).Version
+$biosVersionValue = $biosVersion -replace '\D', ''
+
+$windowsProduct = (Get-WmiObject -class Win32_OperatingSystem).Caption
+$windowsBuildNumber = (Get-WmiObject -class Win32_OperatingSystem).BuildNumber
+
+
+function selfDebug {
+"┌──────────────────────────────────────────────────────────────────────────────────────────────────┐
+│ $selfWidth x $selfHeight                                                       │
+└──────────────────────────────────────────────────────────────────────────────────────────────────┘"
+}
+
 function Show-Menu {
+    Clear-Host
+    generateRow -Width $width -StartChar $startChar$FillChar -EndChar $headerEnd -FillChar $fillChar
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "Q  : Quit | $lastCommand" -Padding
+    generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
+    generateRow -Width $width -StartChar $startChar$FillChar -EndChar $headerEnd -FillChar $fillChar -Content $selfConfig.appName -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "0  : Analyze-system-health   | 10  : Windows 10 Activator      | 20  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "1  : -                       | 11  : Windows 11 Activator      | 21  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "2  : -                       | 12  : -                         | 22  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "3  : -                       | 13  : -                         | 23  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "4  : Policy status           | 14  : -                         | 24  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "5  : -                       | 15  : -                         | 25  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "6  : -                       | 16  : -                         | 26  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "7  : -                       | 17  : -                         | 27  :                           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "8  : Admin-Test              | 18  : -                         | 28  : Wifi-speed-test           " -Padding
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "9  : Check-Bitlocker-Status  | 19  : -                         | 29  : PCI-Express-powersaving   " -Padding
+    generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
+
+    generateRow -Width $width -StartChar $startChar$FillChar -EndChar $headerEnd -FillChar $fillChar
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "[ $(Get-ExecutionPolicy) ][ 💻$(getSerialNumber) ] [$biosVersion]                                  [🛜$(getWifiPercentage)][🔋$(getBatteryPercentage)][ $($currentDate) ]"
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "[ $selfWidth x $selfHeight ] [$(getGPUs)]"
+    generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
+    generateRow -EndChar " " -Content "Performance: $performanceResult"
+}
+
+
+<#function Show-Menu {
+    Clear-Host
+"┌ $($selfConfig.appName) ─────────────────────────────────────────────────────────────────────────────────┐"
+Write-Host "  Q  : Quit | $lastCommand" -ForegroundColor DarkGreen
+"└──────────────────────────────────────────────────────────────────────────────────────────────────┘
+| 0  : Analyze-system-health   | 10  : Windows 10 Activator      | 20  :                           |
+| 1  : -                       | 11  : Windows 11 Activator      | 21  :                           |
+| 2  : -                       | 12  : -                         | 22  :                           |
+| 3  : -                       | 13  : -                         | 23  :                           |
+| 4  : Policy status           | 14  : -                         | 24  :                           |
+| 5  :                         | 15  : -                         | 25  :                           |
+| 6  :                         | 16  : -                         | 26  :                           |
+| 7  :                         | 17  : -                         | 27  :                           |
+| 8  : Admin-Test              | 18  : -                         | 28  : Wifi-speed-test           |
+| 9  : Check-Bitlocker-Status  | 19  : -                         | 29  : PCI-Express-powersaving   |"
+"----------------------------------------------------------------------------------------------------"
+"[ $($currentDate) ][ $(Get-ExecutionPolicy) ][ 🔋$(getBatteryPercentage) ][ 💻$(getSerialNumber) ] [ $biosVersion ] [ 🛜$(getWifiPercentage) ]"
+"[ $selfWidth x $selfHeight ] [$(getGPUs)]"
+#selfDebug
+"----------------------------------------------------------------------------------------------------"
+}#>
+
+<#function Show-Menu {
     Clear-Host
     Write-Host "┌──────────────────────────────────────────────────────────────────────────────────────────────────┐"
     Write-Host "  Q  : Quit | $lastCommand" -ForegroundColor DarkGreen
@@ -21,21 +91,76 @@ function Show-Menu {
     Write-Host "| 8  : Admin-Test              | 18  : -                         | 28  :                           |"
     Write-Host "| 9  : Check-Bitlocker-Status  | 19  : -                         | 29  :                           |"
     Write-Host "----------------------------------------------------------------------------------------------------"
-    Write-Host "[ $($selfConfig.appName) ][ $($currentDate) ][ $(Get-ExecutionPolicy) ]"
-    #Write-Host "[ $selfLocation ]"
+    Write-Host "[ $($selfConfig.appName) v$($selfConfig.appVersion) ][ $($currentDate) ][ $(Get-ExecutionPolicy) ] []"
+    Write-Host "[ $selfWidth x $selfHeight ]"
+    #selfDebug
     Write-Host "----------------------------------------------------------------------------------------------------"
+}#>
+
+function showHelp {
+"********************************************************************************
+Help : helpp
+    SYNPOS: Show This menu
+
+Quit : Q, q, 00
+    SYNPOS: Close Application  
+*********************************************************************************
+"
+Pause
 }
 
-function windowsAdminRequest {
-    param (
-        [Parameter(mandatory=$true)]
-        [string[]] $RequestFor
-    )
-    Write-Host "┌ Request Administrator privileges..." -ForegroundColor Yellow
-    Write-Host "│ Current directroy: [ $selfLocation ]" -ForegroundColor Gray
-    Write-Host "└ Waiting for process..." -ForegroundColor Yellow
-    #Start-Process powershell.exe -Verb runAs -ArgumentList "cd $selfLocation; powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
-    Start-Process powershell.exe -Verb runAs -ArgumentList "powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
+function isWTSession {
+    if ($env:WT_SESSION) {
+        return $true
+    } else {
+        return $false
+    }
+}
+
+function getGPUs {
+    $gpus = @()
+    ((Get-WmiObject Win32_VideoController).Name) | ForEach-Object {
+        if ($_ -eq "Microsoft Basic Display Adapter") {
+            #Write-Host "MISSING DRIVER: $_" -ForegroundColor Red
+            $gpus += $_
+        }
+        if ($_ -eq "Red Hat QXL controller") {
+            #Write-Host "VM: $_" -ForegroundColor Green
+            $gpus += "VM: $_"
+        }
+        else {
+            #Write-Host "OK: $_" -ForegroundColor Green
+            $gpus += "OK: $_"
+        }
+    }
+    return $gpus
+}
+
+function getSerialNumber {
+    $serialNumber = (Get-WmiObject win32_bios).SerialNumber
+    if ($serialNumber -eq $null) {
+        $serialNumber = "VM"
+    }
+    return $serialNumber
+}
+
+function getWifiPercentage {
+    $wifiPercentage = (netsh wlan show interfaces) -Match '^\s+Signal' -Replace '^\s+Signal\s+:\s+',''
+    if ($wifiPercentage -eq $false) {
+        $wifiPercentage = "N/A"
+    }
+    $wifiPercentage = "$wifiPercentage"
+    return $wifiPercentage
+}
+
+function getBatteryPercentage {
+    $batteryPercentage = (Get-WmiObject -Class win32_Battery).estimatedChargeRemaining
+    if ($batteryPercentage -eq $null) {
+        $batteryPercentage = "N/A"
+    } else {
+        $batteryPercentage = "$batteryPercentage%"
+    }
+    return $batteryPercentage
 }
 
 function SetCurrentUserExecutionPolicy {
@@ -47,9 +172,10 @@ function SetCurrentUserExecutionPolicy {
     return Get-ExecutionPolicy
 }
 
+$performanceResult = (Measure-Command {Show-Menu}).TotalSeconds
+
 do {
     Show-Menu
-    
     $shellInput = Read-Host "Input"
     while ($shellInput -eq "") {
         Show-Menu
@@ -66,10 +192,11 @@ do {
         6  {$lastCommand = $lastCommand+": - "}
         7  {$lastCommand = $lastCommand+": 🔥 TODO -> Not implemented"}
         8  {$lastCommand = $lastCommand+': Check-Bitlocker-Status'; windowsAdminRequest -RequestFor "C:\Users\mars\Desktop\git\powershell-lab\src\LAB\todo-elevate_ps1-and-wurcraft\tests\test_no_root_external_powershell.ps1" }
-        9  {$lastCommand = $lastCommand+': Check-Bitlocker-Status'; windowsAdminRequest -RequestFor "$selfLocation\get-bitlocker-status\get-bitlocker-status.ps1" }
+        9  {$lastCommand = $lastCommand+': Check-Bitlocker-Status'; windowsAdminRequest -RequestFor "$selfLocation\extensions\get-bitlocker-status\get-bitlocker-status.ps1" }
         10 {$lastCommand = $lastCommand+': 🔥 TODO -> Not implemented'}
         11 {$lastCommand = $lastCommand+': 🔥 TODO -> Not implemented'}
         '00' { Write-Host "Exit, Goodbye"; Exit 0}
+        'help' {$lastCommand = $lastCommand+': Help'; showHelp }
     }
 } until ($shellInput -eq 'q')
 Write-Host "Exit, Goodbye! "
