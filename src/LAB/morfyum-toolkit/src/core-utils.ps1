9a7e95d9869b73031a7cb9896929fbf7a1dde207
Morfyum <morfyum@gmail.com>
2024-01-27 23:49:34 +0000

    NEW: core-utils for store important tools

---
+++ src/LAB/morfyum-toolkit/src/core-utils.ps1
@@ -0,0 +1,11 @@
+function windowsAdminRequest {
+    param (
+        [Parameter(mandatory=$true)]
+        [string[]] $RequestFor
+    )
+    Write-Output "Request Administrator privileges..."
+    Write-Output "Current directroy: [ $selfLocation ]"
+    Write-Output "Waiting for process..."
+    #Start-Process powershell.exe -Verb runAs -ArgumentList "cd $selfLocation; powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
+    Start-Process powershell.exe -Verb runAs -ArgumentList "powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
+}
