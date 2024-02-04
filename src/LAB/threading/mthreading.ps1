d670d6619431639cabbe5f5318c3e337a3f95026
Morfyum <morfyum@gmail.com>
2024-02-04 01:34:51 +0000

    CLEANUP
    UPDATE: startNewProcess now can recognize .ps1 start and other programs
    
    Still cant stop GUI app processes by ID, like notepad.exe, calc

--- src/LAB/threading/mthreading.ps1
+++ src/LAB/threading/mthreading.ps1
@@ -1,20 +1,40 @@
 
-$path = "C:\Users\mars\Desktop"
+$path = (Get-Location).Path
 $file = "loading.ps1"
 $fullPath = "$path\$file"
 $maxProcesses = 5
-$processLifetime = Get-Random -Maximum 30 -Minimum 10
+#$processLifetime = Get-Random -Maximum 30 -Minimum 10
 
 $processList = @()
 $processMaxLifeInSeconds = @()
 
+if ((Test-Path -Path $fullPath) -eq $false) {
+    Write-Error "Wrong $fullPath. Exit"
+    Exit
+}
 
-function startThisProcess {
-    Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-File", "$fullPath"
-    Start-Sleep -Seconds 2
-    $processId = Get-Process | Where-Object { $_.ProcessName -eq "powershell" -and $_.StartTime -ge (Get-Date).AddMinutes(-1) } | Sort-Object StartTime -Descending
-    $lastProcessId = $processId[0].Id
-    return $lastProcessId
+function startNewProcess {
+    param (
+        [Parameter(mandatory=$true)]
+        [string] $FilePath,
+        [switch] $StartSlow
+    )
+    #Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-File", "$FilePath"
+    if ($FilePath -match ".ps1") {
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-File", "$FilePath"
+        $processPattern = "powershell"
+    } else {
+        Start-Process -FilePath $FilePath
+        $processPattern = $FilePath
+    }
+
+    if ($StartSlow -eq $true) {
+        Start-Sleep -Seconds 6
+    }
+    # processIdArray required, because we need to calculate what process started. Currently LAST ID is the last started process.
+    $processIdArray = Get-Process | Where-Object { $_.ProcessName -eq $processPattern -and $_.StartTime -ge (Get-Date).AddMinutes(-1) } | Sort-Object StartTime -Descending
+    $processId = $processIdArray[0].Id
+    return $processId
 }
 
 $counter = 0
@@ -23,53 +43,48 @@ while ($true) {
     $index = 0
     if ($processList.Length -lt $maxProcesses) {
         
-        $currentProcessId = startThisProcess
-        $processLifetime = Get-Random -Maximum 60 -Minimum 15
+        $currentProcessId = startNewProcess -FilePath $fullPath # "notepad.exe" # $fullPath
+        $curremtProcessLifetime = 10 #Get-Random -Maximum 20 -Minimum 15
         
-        Write-Output "ProcessID: [$currentProcessId] => New Process for $processLifeTime seconds..."
+        Write-Output "ProcessID: [$currentProcessId] => New Process for [$curremtProcessLifetime] seconds..."
         $processList += $currentProcessId
-        $processMaxLifeInSeconds +=  $processLifeTime
+        $processMaxLifeInSeconds +=  $curremtProcessLifetime
         $counter++
-        Start-Sleep -Seconds 3
+        Start-Sleep -Seconds 1
     } else {
-        Clear-Host
-        Write-Output "Watching... counter[$counter] i[$index]"
+        #Clear-Host
+        [Console]::Clear()
+        #Write-Output "Watching... [$counter] started."
+        [Console]::WriteLine("Watching [ $counter ] started process...")
         $processList | ForEach-Object {
-            Write-Output "Process: [$_] $($processMaxLifeInSeconds[$index])"
+            #Write-Output "Process: [$_] $($processMaxLifeInSeconds[$index])"
+            [Console]::WriteLine("Process: [$_] $($processMaxLifeInSeconds[$index])")
             if ($($processMaxLifeInSeconds[$index]) -gt 0) {
-                $processMaxLifeInSeconds[$index] = $($processMaxLifeInSeconds[$index])-1
+                $processMaxLifeInSeconds[$index]--
+
+                #if ($(Get-Process -Id $($processList[$index]) -ErrorAction SilentlyContinue) -eq $null ) {
+                #    $processList[$index] = "Stopped"
+                #    $processMaxLifeInSeconds[$index] = 0
+                #}
             }
             if ( $processMaxLifeInSeconds[$index] -eq 0 -and $processList[$index] -ne "Stopped") {
                 #Write-Host "Start New process..."
-                Write-Output "Stop Process... $($processList[$index])"
-                Stop-Process -Id $($processList[$index])
+                #Write-Output "Stop Process... $($processList[$index])"
+                [Console]::WriteLine("Stop Process... $($processList[$index])")
+
+                if ($(Get-Process -Id $($processList[$index]) -ErrorAction SilentlyContinue) -eq $null ) {
+                    #pass
+                }
+                else {
+                    #$thisProcessName = (Get-Process -Id $($processList[$index])).ProcessName
+                    Stop-Process -Id $($processList[$index]) -Force
+                    #Stop-Process -Name $thisProcessName
+                    #Start-Sleep -Seconds 1
+                }
                 $processList[$index] = "Stopped"
-            } else {
-                # pass
             }
             $index++
         }
         Start-Sleep -Seconds 1
     }
 }
-
-
-
-
-<#
-while ($processList.Length -lt $maxProcesses) {
-    Write-Output "Start New Process $($porcessList.Length)"
-    $processList += "$($processList.Length)"
-    Write-Output "$processList"
-}#>
-
-# Ellenőrzés, hogy létezik-e még a folyamat
-
-<#if (Get-Process -Id $processId -ErrorAction SilentlyContinue) {
-    Write-Output "Running: $processId"
-} else {
-    Write-Output "Stopped: $processId"
-}
-#>
-
-
