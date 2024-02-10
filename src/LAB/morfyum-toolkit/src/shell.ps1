12dd156c8e19de321d6a3f84d1134b7421405ca7
Morfyum <morfyum@gmail.com>
2024-02-10 20:05:55 +0000

    Read shell-config files and test paths.
    Now can execute shell-config menu elements
    
    Removed oldmenu and moved into UI.ps1

--- src/LAB/morfyum-toolkit/src/shell.ps1
+++ src/LAB/morfyum-toolkit/src/shell.ps1
@@ -4,16 +4,18 @@
 
 # SELF CONFIGURATION AND VARIABLES 
 $selfLocation = (Get-Location).Path
+#$selfDirName = $selfLocation | Split-Path -Leaf
 $selfConfig = Get-Content -Path "$selfLocation\shell-config.json" | Out-String | ConvertFrom-Json
 $selfWidth = $host.UI.RawUI.WindowSize.Width
 $selfHeight = $host.UI.RawUI.WindowSize.Height
 $host.UI.RawUI.WindowTitle = "$($selfConfig.appName)"
 $host.UI.RawUI.foregroundColor = $($selfConfig.fontColor)
 $currentDate = Get-Date -Format "yyyy-MM-dd"
-$lastCommand = ""
+$lastCommand = "show available commands: getHelp"
 $width = 100  #100
+$promptName = "Input"  # (Get-Location).Path | Split-Path -Leaf
 
-$menuLength = $($selfConfig.menu).Length
+#$menuLength = $($selfConfig.menu).Length
 
 
 # EXTERNAL
@@ -23,12 +25,20 @@ $biosVersionValue = $biosVersion -replace '\D', ''
 $windowsProduct = (Get-WmiObject -class Win32_OperatingSystem).Caption
 $windowsBuildNumber = (Get-WmiObject -class Win32_OperatingSystem).BuildNumber
 
-#$IPv4Address = (Get-NetIPConfiguration).IPv4Address.IPAddress
-#$ethernetMacAddress = (Get-NetAdapter -Name "Ethernet").MacAddress
+$IPv4Address = (Get-NetIPConfiguration).IPv4Address.IPAddress
+$ethernetMacAddress = (Get-NetAdapter -Name "Ethernet").MacAddress
 
-function selfDebug {
-    # pass
+$commands = @{}
+$commandsIndex = 0
+$selfConfig.menu | ForEach-Object {
+    $thisPath = "$($_.filePath)/$($_.file)"
+    if ((Test-Path -Path $thisPath) -eq $true) {
+        #. $thisPath
+        $commands[$commandsIndex] = $thisPath
+        $commandsIndex++
+    }
 }
+
 #<#
 function Show-Menu {
     #Clear-Host # REPLACED for improve performance! + ~0.02-0,06 ms
@@ -37,13 +47,13 @@ function Show-Menu {
     generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "Q  : Quit | $lastCommand" -Padding
     generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
     
-    Write-Output "Menu length: [ $menuLength ]"
+    #Write-Output "Menu length: [ $menuLength ]"
     $menuIndex = 0
-    $maxNameLength = 24
-    $contentFiller = " "
-    $contentArray = @()
+    #$maxNameLength = 24  # maxNameLength countered by generateRow for prevent user error
+    #$contentFiller = " "
+    #$contentArray = @()
 
-    generateRow -Width $width -StartChar $headerStart$FillChar -EndChar $headerEnd -FillChar $fillChar
+    generateRow -Width $width -StartChar "$headerStart$FillChar" -Content $($selfConfig.appName) -EndChar $headerEnd -FillChar $fillChar -Padding
 
     <# 0 1 2
        3 4 5
@@ -72,46 +82,39 @@ function Show-Menu {
     $selfConfig.menu | ForEach-Object {
         $columns = 3
         $roundedWidth = [Math]::Ceiling($width / $columns)
-        $maxRow = [int]$menuLength/$columns
+        #[int]$maxRow = $menuLength/$columns
         $counter++
 
-        $worker += generateRow -Width  $roundedWidth -StartChar "" -Content "$menuIndex : $($_.name) - [$counter] MAXROW: $([int]$maxRow)" -EndChar "|" -Padding
+        #$worker += generateRow -Width  $roundedWidth -StartChar "" -Index $menuIndex -Content " : $($_.name) - [$counter] MAXROW: $($maxRow)" -EndChar "| " -Padding
+        $worker += generateRow -Width  $roundedWidth -StartChar "" -Index $menuIndex -Content " : $($_.name)" -EndChar "| " -Padding
         if ($counter%$columns -eq 0) {
             #echo "% : $($counter%$columns)"
-            $Content = generateRow -Width $width -StartChar "|" -Content $worker -EndChar "|"
+            $Content = generateRow -Width $width -StartChar "$pipeChar" -Content $worker -EndChar "$pipeChar"
             $Content
             $worker = ""
             $counter = 0
         }
         $menuIndex++        
     }
-    $Content = generateRow -Width $width -StartChar "|" -Content $worker -EndChar "|"
-    $Content
+    if ($worker -notmatch "^\s*$"){
+        $Content = generateRow -Width $width -StartChar "$pipeChar" -Content $worker -EndChar "$pipeChar"
+        $Content
+    }
 
     generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
     
 
-    generateRow -Width $width -StartChar $headerStart$FillChar -EndChar $headerEnd -FillChar $fillChar -Content $selfConfig.appName -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "0  : Analyze-system-health   | 10  : Windows 10 Activator      | 20  :                           " -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "1  : -                       | 11  : Windows 11 Activator      | 21  :                           " -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "2  : -                       | 12  : -                         | 22  :                           " -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "3  : -                       | 13  : -                         | 23  :                           " -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "4  : Policy status           | 14  : -                         | 24  :                           " -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "5  : -                       | 15  : -                         | 25  :                           " -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "6  : -                       | 16  : -                         | 26  :                           " -Padding
-    #Write-Host "----------------------------------------------------------------------------------------------------"
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "7  : -                       | 17  : -                         | 27  : 12345123451234512345-012356789dddd" -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "8  : Admin-Test              | 18  : -                         | 28  : Wifi-speed-test           " -Padding
-    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "9  : Check-Bitlocker-Status  | 19  : -                         | 29  : PCI-Express-powersaving  dddd" -Padding
-    generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
-
-    generateRow -Width $width -StartChar $headerStart$FillChar -EndChar $headerEnd -FillChar $fillChar
-    #generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "[ $(Get-ExecutionPolicy) ][ 💻$(getSerialNumber) ] [$biosVersion]                                  [🛜$(getWifiPercentage)][🔋$(getBatteryPercentage)][ $($currentDate) ]"
-    #generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "[ $selfWidth x $selfHeight ] [$(getGPUs)]"
-    generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
+    #
+    #
     #generateRow -EndChar " " -Content "Performance: $performanceResult1 ms | $performanceResult2 ms"
-}#>
 
+    #generateRow -Width $width -StartChar $headerStart$FillChar -EndChar $headerEnd -FillChar $fillChar
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "[ $(Get-ExecutionPolicy) ][ 💻$(getSerialNumber) ] [$biosVersion]                                 [🛜$(getWifiPercentage)][🔋$(getBatteryPercentage)][ $($currentDate) ]"
+    generateRow -Width $width -StartChar $pipeChar -EndChar "$pipeChar" -Content "Performance: $performanceResult1 ms | $performanceResult2 ms"
+    generateRow -Width $width -StartChar $pipeChar -EndChar $pipeChar -FillChar " " -Content "[ $selfWidth x $selfHeight ] [$(getGPUs)]"
+    generateRow -Width $width -StartChar $footerStart -EndChar $footerEnd -FillChar $fillChar
+
+}#>
 
 <#function Show-Menu {
     Clear-Host
@@ -159,16 +162,17 @@ function Show-Menu {
     Write-Host "----------------------------------------------------------------------------------------------------"
 }#>
 
-function showHelp {
-"********************************************************************************
-Help : helpp
-    SYNPOS: Show This menu
+function getHelp {
+    "
+    ********************************************************************************
+    Help : helpp
+        SYNPOS: Show This menu
 
-Quit : Q, q, 00
-    SYNPOS: Close Application  
-*********************************************************************************
-"
-Pause
+    Quit : Q, q, 00
+        SYNPOS: Close Application  
+    *********************************************************************************
+    "
+Pause 
 }
 
 function isWTSession {
@@ -240,14 +244,56 @@ $performanceResult1 = (Measure-Command {Show-Menu}).Milliseconds
 do {
     $performanceResult2 = (Measure-Command {Show-Menu}).Milliseconds
     Show-Menu
-    $shellInput = Read-Host "Input"
+    $shellInput = Read-Host "$promptName"
     while ($shellInput -eq "") {
         Show-Menu
-        $shellInput = Read-Host "Input"
+        $shellInput = Read-Host "$promptName"
     }
     $lastCommand = $shellInput
+
+
+    if ($shellInput -match '^\d+$') {
+        $shellInput = [int]$shellInput
+        try {
+            Write-Host "Internal Call : $($commands[$shellInput])" -ForegroundColor Green
+            Invoke-Expression $commands[$shellInput]
+            $lastCommand = "$shellInput : $($commands[$shellInput])"
+        } catch {
+            $lastCommand = "$shellInput : [$($commands[$shellInput])] Invalid value"
+        }
+    } else {
+        try {
+            Invoke-Expression $shellInput
+            #pause
+        } catch {
+            $lastCommand = "Invalid Command : $shellInput"
+        }
+        
+    }
+
     switch ($shellInput) {
-        0  {$lastCommand = $lastCommand+': 🔥 TODO -> Not implemented'}
+        10000  {
+
+            function executeExtension {
+                param (
+                    [Parameter(Mandatory=$true)]
+                    [strig] $shellInput
+                )
+                if ($shellInput -eq [int]) {
+                    Write-Host "INT"
+                }
+                $execPath = $selfConfig.menu[$shellInput].filePath
+                $execFile = $selfConfig.menu[$shellInput].file
+                $execFullPath = "$execPath/$execFile"
+            }
+
+            $lastCommand = $lastCommand+": 0 path: [$execFullPAth])"
+            #if ((Test-Path -Path $execFullPath) -eq $true) {
+            #. $execFullPath
+            #exitApp
+            #}
+
+           }
         1  {$lastCommand = $lastCommand+': TEST '}
         2  {$lastCommand = $lastCommand+": You chose option #2"}
         3  {$lastCommand = $lastCommand+': You chose option #3'}
@@ -260,7 +306,7 @@ do {
         10 {$lastCommand = $lastCommand+': 🔥 TODO -> Not implemented'}
         11 {$lastCommand = $lastCommand+': 🔥 TODO -> Not implemented'}
         '00' { Write-Host "Exit, Goodbye"; Exit 0}
-        'help' {$lastCommand = $lastCommand+': Help'; showHelp }
+        #'showhelp' {$lastCommand = $lastCommand+': Help'; showHelp }
     }
 } until ($shellInput -eq 'q')
 Write-Host "Exit, Goodbye! "
