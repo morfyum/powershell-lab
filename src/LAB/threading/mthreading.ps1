5699c6089ab4dc249dbb717e8ebe2a2073d01cbd
Morfyum <morfyum@gmail.com>
2024-02-10 22:52:16 +0000

    New: Dictionary for processes
    New dictionary manager functions: GetDictState, SetDictState

--- src/LAB/threading/mthreading.ps1
+++ src/LAB/threading/mthreading.ps1
@@ -1,12 +1,37 @@
-
 $path = (Get-Location).Path
 $file = "loading.ps1"
 $fullPath = "$path\$file"
-$maxProcesses = 5
+$maxProcesses = 3
 #$processLifetime = Get-Random -Maximum 30 -Minimum 10
-
 $processList = @()
 $processMaxLifeInSeconds = @()
+$dictionary = @{}
+
+class ProcessData {
+    [int] $ProcessID
+    [string] $ProcessState
+    [string] $ProcessLifetime
+    [string] $ProcessRemainingLife
+    [string] $ProcessResult
+}
+#$CurrentProcessData = [ProcessData]::new()
+
+function GetDictState {
+    foreach ($Key in $dictionary.Keys) {
+        Write-Host "Key: $Key Value: $($dictionary[$Key])"
+    }
+}
+
+function SetDictValue {
+    param (
+        [int] $Key
+    )
+    if ((Test-Path ".\proc\$Key" -ErrorAction SilentlyContinue) -ne $false  ) {
+        $dictionary[$Key] = Get-Content -Path ".\proc\$Key"
+        Remove-Item -Path ".\proc\$Key"
+    }
+}
+
 
 if ((Test-Path -Path $fullPath) -eq $false) {
     Write-Error "Wrong $fullPath. Exit"
@@ -21,7 +46,8 @@ function startNewProcess {
     )
     #Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-File", "$FilePath"
     if ($FilePath -match ".ps1") {
-        Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-File", "$FilePath" #-WindowStyle hidden
+        #Start-Process -FilePath "powershell.exe" -ArgumentList "-NoExit", "-File", "$FilePath" #-WindowStyle hidden
+        Start-Process -FilePath "powershell.exe" -ArgumentList "-File", "$FilePath" #-WindowStyle hidden
         $processPattern = "powershell"
     } else {
         Start-Process -FilePath $FilePath
@@ -41,36 +67,37 @@ while ($true) {
     $index = 0
     if ($processList.Length -lt $maxProcesses) {
         
-        $currentProcessId = startNewProcess -FilePath $fullPath # "notepad.exe" # $fullPath
+        $currentProcessId = startNewProcess -FilePath  .\dummy-process.ps1 #$fullPath
         $curremtProcessLifetime = 20 #Get-Random -Maximum 20 -Minimum 15
         
         Write-Output "ProcessID: [$currentProcessId] => New Process for [$curremtProcessLifetime] seconds..."
         $processList += $currentProcessId
         $processMaxLifeInSeconds +=  $curremtProcessLifetime
+
+        $dictionary.Add($currentProcessId, $null)
+
         $counter++
         Start-Sleep -Seconds 1
     } else {
-        #Clear-Host
         [Console]::Clear()
-        #Write-Output "Watching... [$counter] started."
         [Console]::WriteLine("Watching [ $counter ] started process...")
+        GetDictState # -Key $_
         $processList | ForEach-Object {
-            #Write-Output "Process: [$_] $($processMaxLifeInSeconds[$index])"
-            [Console]::WriteLine("Process: [$_] $($processMaxLifeInSeconds[$index])")
+            [Console]::WriteLine("Process[$index]: [$_] $($processMaxLifeInSeconds[$index])")
             if ($($processMaxLifeInSeconds[$index]) -gt 0) {
                 $processMaxLifeInSeconds[$index]--
+                SetDictValue -Key $_
+
                 if ($(Get-Process -Id $($processList[$index]) -ErrorAction SilentlyContinue) -eq $null ) {
                     $processList[$index] = "Stopped"
                     $processMaxLifeInSeconds[$index] = 0
                 }
             }
             if ( $processMaxLifeInSeconds[$index] -eq 0 -and $processList[$index] -ne "Stopped") {
-                #Write-Host "Start New process..."
-                #Write-Output "Stop Process... $($processList[$index])"
                 [Console]::WriteLine("Stop Process... $($processList[$index])")
 
                 if ($(Get-Process -Id $($processList[$index]) -ErrorAction SilentlyContinue) -eq $null ) {
-                    #pass
+                    #Porcess exitest by self
                 }
                 else {
                     #$thisProcessName = (Get-Process -Id $($processList[$index])).ProcessName
@@ -80,6 +107,11 @@ while ($true) {
                 }
                 $processList[$index] = "Stopped"
             }
+
+            if ($processList[$index] -eq "Stopped") {
+                Write-Host "Ready To start New process in ID [$index]"
+            }
+
             $index++
         }
         Start-Sleep -Seconds 1
