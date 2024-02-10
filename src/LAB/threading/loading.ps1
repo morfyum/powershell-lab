76a83fccc4efebfc6d420fdf29f1864849a0275e
Morfyum <morfyum@gmail.com>
2024-02-10 22:52:47 +0000

    cleanup

--- src/LAB/threading/loading.ps1
+++ src/LAB/threading/loading.ps1
@@ -8,11 +8,12 @@ $array= @(".  ", ":  ", ":. ", ":: ", "::.", ":::", "::.", ":: ", ":. ", ":  ", 
 
 
 function showLoading {
+    $array= @(".  ", ":  ", ":. ", ":: ", "::.", ":::", "::.", ":: ", ":. ", ":  ", ".  ", "   ")
     foreach ($currentItemName in $array) {
         #Clear-Host
         [Console]::Clear()
         #Write-Output "[$currentItemName] Progress..."
-        [Console]::WriteLine("[$currentItemName] Progress...")
+        [Console]::WriteLine("[$currentItemName] $($PID) Progress...")
         Start-Sleep -Seconds 1
         #Start-Sleep -Milliseconds 150
     }
