21089a8bc24e0d4796a50aab50f1ffa9e8839cfe
Morfyum <morfyum@gmail.com>
2024-02-11 01:43:55 +0000

    Replace basic start-process with cd  version

--- src/LAB/morfyum-toolkit/src/core-utils.ps1
+++ src/LAB/morfyum-toolkit/src/core-utils.ps1
@@ -7,6 +7,7 @@ function windowsAdminRequest {
     Write-Output "Current directroy: [ $selfLocation ]"
     Write-Output "$RequestFor"
     Write-Output "Waiting for process..."
-    #Start-Process powershell.exe -Verb runAs -ArgumentList "cd $selfLocation; powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
-    Start-Process powershell.exe -Verb runAs -ArgumentList "powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
+    Start-Process powershell.exe -Verb runAs -ArgumentList "cd $selfLocation; powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
+    #Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Unrestricted -Force
+    #Start-Process powershell.exe -Verb runAs -ArgumentList "powershell.exe -ExecutionPolicy RemoteSigned $RequestFor" -Wait
 }
