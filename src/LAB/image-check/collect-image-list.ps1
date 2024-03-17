25cbae5c3253886c34e1cd8b224530c6a03ae184
Morfyum <morfyum@gmail.com>
2024-03-17 23:09:53 +0000

    add: findOnShare what return true or false from by server file index (probably too slow)
    CheckImageInList : performance improvement

--- src/LAB/image-check/collect-image-list.ps1
+++ src/LAB/image-check/collect-image-list.ps1
@@ -1,22 +1,40 @@
 
-$Global:serverPath = "./server"
-$Global:imageListFile = "./imageList.txt"
+$serverPath = "./server"
+$imageListFile = "./imageList.txt"
+
+function findOnShare {
+    param (
+        [string] $SharePath,
+        [string] $SkuNumber
+    )
+    $hasImage = $false
+    Get-ChildItem $SharePath | ForEach-Object {
+        if("123456789#ABZ" -eq $_){
+            $hasImage = $true
+            return $hasImage
+        }
+    }
+    return $hasImage
+}
 
 function MakeImageList {
     param (
         [string] $Location
     )
+    $currentDate = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+    Write-Host "Last Update: $lastModified"
     $imageList = @()
+    $imageList += $currentDate
     (Get-ChildItem -Path $Location) | ForEach-Object {
         if ($_.Name -match '.........#[A-Z][A-Z][A-Z]') {
-            Write-Host " - [$($_.Name)]"
+            Write-Host "Add to list: $($_.Name)"
             $imageList += $_.Name
         }
     }
     return $imageList
 }
 
-MakeImageList -Location $Global:serverPath > $Global:imageListFile
+MakeImageList -Location $serverPath > $imageListFile
 
 
 function CheckImageInList {
@@ -29,9 +47,10 @@ function CheckImageInList {
         #Write-Host "$SkuNumber ? $_"
         if ($_ -eq $SkuNumber) {
             $hasImage = $true
+            return $hasImage
         }
     }
-    return $hasImage 
+    return $hasImage
 }
 
-CheckImageInList -ImageList $Global:imageListFile -SkuNumber "AA3456789#UUZ"
+CheckImageInList -ImageList $imageListFile -SkuNumber "AA3456789#UUZ"
