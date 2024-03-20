a9aab106c2d2c6b91ae4c10b2088b2a38af35b7d
Morfyum <morfyum@gmail.com>
2024-03-20 18:54:34 +0000

    update bat file with start.ps1

--- src/LAB/debitlocker/debitlocker.bat
+++ src/LAB/debitlocker/debitlocker.bat
@@ -1,4 +1,4 @@
 echo off
 cd .\src\
-powershell.exe -ExecutionPolicy RemoteSigned -File .\main.ps1
-::powershell.exe -executionpolicy Bypass -File  ".\main.ps1" -Command "Start-Process powershell.exe"
+powershell.exe -ExecutionPolicy RemoteSigned -File .\start.ps1
+::powershell.exe -executionpolicy Bypass -File  ".\start.ps1" -Command "Start-Process powershell.exe"
