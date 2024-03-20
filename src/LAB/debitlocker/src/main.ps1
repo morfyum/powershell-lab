1f0463c4d379be77b6515998ab9f67315cd8ff00
Morfyum <morfyum@gmail.com>
2024-03-20 18:55:15 +0000

    Now main.ps1 use debitlocker sources

--- src/LAB/debitlocker/src/main.ps1
+++ src/LAB/debitlocker/src/main.ps1
@@ -1,14 +1,6 @@
-$selfLocation = (Get-Location).Path
-function WindowsAdminRequest {
-    param (
-        [Parameter(mandatory=$true)]
-        [string[]] $RequestFor
-    )
-    Write-Output "Request Administrator privileges..."
-    Write-Output "Current directroy: [ $selfLocation ]"
-    Write-Output "$RequestFor"
-    Write-Output "Waiting for process..."
-    Set-Location $selfLocation
-    Start-Process powershell.exe -Verb runAs -ArgumentList "cd $selfLocation; powershell.exe -ExecutionPolicy ByPass -File $RequestFor"
-}
-windowsAdminRequest -RequestFor "debitlocker.ps1"
+. .\debitlocker.ps1
+
+$STATES = GetBitlockerStatus
+#$STATES.Add("D:", 100)
+ShowState -VolumeList $STATES
+Pause
