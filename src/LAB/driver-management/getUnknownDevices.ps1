7e19af4af0933020b2e3ee1d524979a05df653f0
morfyum <morfyum@gmail.com>
2024-06-09 20:08:15 -0700

    Can get unknown devices, make database, and export into JSON

--- src/LAB/driver-management/getUnknownDevices.ps1
+++ src/LAB/driver-management/getUnknownDevices.ps1
@@ -1,23 +1,84 @@
 
-class UnknownDeviceInfo {
-    [string] $VendorID
-    [string] $DeviceID
+class UnknownDeviceModel {
+    [string] $DeviceType = $null
+    [string] $VendorID = $null
+    [string] $DeviceID = $null
 }
 
-function GetUnknownDevices {
+$unknownDeviceList = @()
+
+function GenerateUnknownDeviceArray {
+    $unknownDeviceStringList = @()
     Get-WmiObject Win32_PnPentity | Where-Object {$_.ConfigManagerErrorCode -ne 0} | Select-Object Status, ConfigManagerErrorCode, Name, DeviceID | ForEach-Object {
         Write-Host "Found Unknown device: $($_.DeviceID)" -ForegroundColor Yellow
+        $unknownDeviceStringList += $($_.DeviceID)
     }
+    
+    return $unknownDeviceStringList
 }
 
-
-function TestPathErrorHandler {
+function GetDeviceType {
     param (
-        [Parameter(mandatory=$true)]
-        [string] $Path,
-        [switch] $ShowSuccess
+        [Parameter(Mandatory=$true)]
+        [string] $string
     )
+    $deviceType = $string.Substring(0, $string.IndexOf("\"))
+    #Write-Host "  DeviceType : $deviceType"
+    return $deviceType
+}
 
+function GetVendorID {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $string
+    )
+    $match = $string -match "(?<=VEN_)\w+"
+    if ($match -eq $true) {
+        $vendorID = $Matches[0]
+        #Write-Host "VendorID   : $($VendorID)" -ForegroundColor Green
+    }
+    return $vendorID
+}
 
-GetUnknownDevices
+function GetDeviceID {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $string
+    )
+    $match = $string -match "(?<=DEV_)\w+"
+    if ($match -eq $true) {
+        $deviceID = $Matches[0]
+        #Write-Host "DeviceID   : $($deviceID)" -ForegroundColor Green
+    }
+    return $deviceID
+}
 
+########## ########## APP ########## ##########
+$unknownDevicesArray = GenerateUnknownDeviceArray
+
+$unknownDevicesArray | ForEach-Object {
+    Write-Host "OK": $_ -ForegroundColor Green
+
+    $deviceType = GetDeviceType -string $_
+    $vendorID = GetVendorID -string $_
+    $deviceID = GetDeviceID -string $_
+
+    Write-Host "- Type   : $deviceType"
+    Write-Host "- Vendor : $vendorID"
+    Write-Host "- Device : $deviceID"
+
+    $unknownDeviceInfo = [UnknownDeviceModel]::new()
+    $unknownDeviceInfo.DeviceType = $deviceType
+    $unknownDeviceInfo.VendorID = $vendorID
+    $unknownDeviceInfo.DeviceID = $deviceID
+    $unknownDeviceList += $unknownDeviceInfo
+
+}
+
+$unknownDeviceList
+
+$jsonString = $unknownDeviceList | ConvertTo-Json
+$jsonString | Out-File -FilePath "unknownDeviceList.json" 
+
+# READ CONTENT IN THE FUTURE:
+# $jsonData = Get-Content -Path .\unknownDeviceList.json | ConvertFrom-Json
