acea7dfab33dac537441fec7c51865ace5769f4d
Mrofyum <morfyum@gmail.com>
2024-07-10 20:27:33 +0200

    hotfix: now install olny unknown device drivers

--- src/LAB/driver-management/main.ps1
+++ src/LAB/driver-management/main.ps1
@@ -1,12 +1,16 @@
-# Control process for Unknown Device Handler
-# github.com/morfyum
+# Unknown Device Handler by github.com/morfyum
 . .\infInstall.ps1
 
 # $jsonData = Get-Content -Path .\unknownDeviceList.json | ConvertFrom-Json
 
 foreach ($infFile in $infFiles) {
     #Write-Host "*** INF LOOKUP ***" -ForegroundColor Cyan
-    CheckDeviceIDInFile -FilePath $($infFile.FullName) -VendorID "1B36" -DeviceID "0100"
-    #pnputil.exe /add-driver $infFile.FullName /install
+    
+    CheckDeviceIDInFile -FilePath $($infFile.FullName) -VendorID "1B36" -DeviceID "0100" | ForEach-Object {
+        if ($_ -eq $true) {
+            Write-Host "INSTALL"
+            pnputil.exe /add-driver $infFile.FullName /install
+        }
+    }
 }
 
