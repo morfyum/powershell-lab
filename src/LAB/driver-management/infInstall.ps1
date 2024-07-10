1387d952b771f8565cc207c0369e6b88f45e53ec
Mrofyum <morfyum@gmail.com>
2024-07-10 18:04:21 +0200

    Cleanup.
    Show only required inf files
    
    foreach moved into main.ps1

--- src/LAB/driver-management/infInstall.ps1
+++ src/LAB/driver-management/infInstall.ps1
@@ -1,8 +1,9 @@
+# github.com/morfyum
 # TEST ON VM: PCI\VEN_1B36&DEV_0100&SUBSYSTEM_...
 
 $driverDirectory = "C:\Users\mars\Downloads\extracted-drivers\"
 $infFiles = Get-ChildItem -Path $driverDirectory -Recurse -Filter *.inf
-$testInfPath = "C:\Users\mars\Downloads\extracted-drivers\spice-guest-tools-latest\drivers\qxl\2k8R2\amd64\qxl.inf"
+#$testInfPath = "C:\Users\mars\Downloads\extracted-drivers\spice-guest-tools-latest\drivers\qxl\2k8R2\amd64\qxl.inf"
 
 function CheckDeviceIDInFile {
     param (
@@ -12,15 +13,18 @@ function CheckDeviceIDInFile {
     )
     $fileContent = Get-Content -Path $FilePath
     $pattern = "VEN_$VendorID&DEV_$DeviceID"
-    Write-Host "|<- $FilePath" -ForegroundColor Yellow
-    Write-Host "|-> [$pattern]" -ForegroundColor Yellow
+    #Write-Host "|<- $FilePath" -ForegroundColor Yellow
+    #Write-Host "|-> [$pattern]" -ForegroundColor Yellow
     foreach ($line in $FileContent) {
         if ($line -match $pattern) {
+            Write-Host "|<- $FilePath" -ForegroundColor Yellow
+            Write-Host "|-> [$pattern]" -ForegroundColor Yellow
             Write-Host "$line" -ForegroundColor Green
+
             return $true
         }
     }
-    return $false
+    return $false | Out-Null
 }
 
 function InstallInf {
@@ -33,11 +37,4 @@ function InstallInf {
         Write-Host "Install: $($infFile.FullName)"
         pnputil /add-driver $infFile.FullName /install
     }
-}
-
-
-foreach ($infFile in $infFiles) {
-    Write-Host "*** $($infFile.FullName)" -ForegroundColor Red
-    CheckDeviceIDInFile -FilePath $($infFile.FullName) -VendorID "1B36" -DeviceID "0100"
-    #pnputil.exe /add-driver $infFile.FullName /install
 }
