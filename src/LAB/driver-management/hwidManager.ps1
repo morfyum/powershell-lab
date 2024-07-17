6254028b753335dfd605023f28fd73e3a493dc66
Mrofyum <morfyum@gmail.com>
2024-07-17 22:46:20 +0200

    Add error handling

--- src/LAB/driver-management/hwidManager.ps1
+++ src/LAB/driver-management/hwidManager.ps1
@@ -1,15 +1,23 @@
 . .\get-unknown-devices.ps1
 
 $driverDirectory = "C:\Users\mars\Downloads\extracted-drivers\spice-guest-tools-latest"
-
 $unknownDevices = GetUnknownDevices
 $infFiles = Get-ChildItem -Path $driverDirectory -Recurse -Filter *.inf
+
+# Error handling before run
+if ((Test-Path -Path $driverDirectory) -eq $false) {
+    Write-Host "Missing driverDirectory path: $driverDriectory. Exit" -ForegroundColor Red
+    Exit 1
+}
 
 if ($unknownDevices.Length -eq 0) {
     Write-Host "All device ready to use. Exit" -ForegroundColor Green
     Exit 0
 }
+if ($infFiles.Length -eq 0) {
+    Write-Host "Missing .inf file list. Exit" -ForegroundColor Red
+    Exit 1
+}
 
-
-
+# Runk without -Install to check What files are matched with unknown devices HardwareIDs
 PNPInstallProcess -Install
