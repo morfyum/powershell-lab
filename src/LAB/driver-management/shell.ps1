4315c7393af4701c8840cde2095f71a05b4258b4
Mrofyum <morfyum@gmail.com>
2024-07-25 19:12:18 +0200

    add shell.ps1 for test and use finctions

---
+++ src/LAB/driver-management/shell.ps1
@@ -0,0 +1,14 @@
+. .\saveDrivers.ps1
+. .\get-unknown-devices.ps1
+
+function prompt {
+"########################################################################################################################
+# SaveDrivers [-DryRun] [[-SaveLocation] <String>] [-CreateLocation]
+# GetUnknownDevices
+# CheckDeviceIDInFile [[-FilePath] <string>] [[-Pattern] <string>] [-Silent]
+# PNPInstallProcess [[-DefaultID] <string>] [-Install]
+########################################################################################################################
+commands_$ "
+}
+
+Write-Host "Interactive PowerShell session started. Custom prompt set to 'commands_$ '."
