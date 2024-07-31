655554c9d7ca2490b9283a20127538ad3ba95d26
Mrofyum <morfyum@gmail.com>
2024-07-31 20:12:47 +0200

    - More verpose PNPInstallProcess
    - return when UnknownDeviceList -eq 0

--- src/LAB/driver-management/src/Unknown-Device-Handler-Tools.ps1
+++ src/LAB/driver-management/src/Unknown-Device-Handler-Tools.ps1
@@ -71,10 +71,14 @@ function PNPInstallProcess {
     )
     if ($ByID -eq "") {
         $ByID = "HardwareID"
-        Write-Host "Using [$byID] by default. < HardwareID | CompatibleID >"
+        Write-Host "[i] PNPInstallProcess: Using [$byID] by default. < HardwareID | CompatibleID >" -ForegroundColor Cyan
     } else {
-        Write-Host "Using [$ByID]"
+        Write-Host "[i] PNPInstallProcess: Using [$ByID]. < HardwareID | CompatibleID >" -ForegroundColor Cyan
     }
+    if ($($UnknownDeviceList.Length) -eq 0) {
+        return "[-] ??? return because Not found Unknown Device !!!"
+    }
+    pause
     if ($Install -eq $false) {
         Write-Host "=============================================================" -ForegroundColor Cyan
         Write-Host "*** Running in TEST mode. Add -Install to install drivers ***" -ForegroundColor Cyan
@@ -89,10 +93,75 @@ function PNPInstallProcess {
             if ($result -eq $true) {
                 Write-Host " - Add : $pattern" -ForegroundColor Green
                 if ($Install -eq $true) {
-                    Write-Host "*** Install driver ***" -ForegroundColor Green
+                    Write-Host "*** Install driver: $($infFile.FullName)***" -ForegroundColor Green
                     pnputil.exe /add-driver $($infFile.FullName) /install /force
                     $UnknownDeviceList = GetUnknownDevices  # New Line for refresh unknown device list after install driver
-                    return
+                }
+            } else {
+                Write-Host " - skip: $pattern" -ForegroundColor Gray
+            }
+        }
+    }
+}
+
+function GetVendorID {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $string
+    )
+    $match = $string -match "(?<=VEN_)\w+"
+    if ($match -eq $true) {
+        $vendorID = $Matches[0]
+        #Write-Host "VendorID   : $($VendorID)" -ForegroundColor Green
+    }
+    return $vendorID
+}
+
+function GetDeviceID {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string] $string
+    )
+    $match = $string -match "(?<=DEV_)\w+"
+    if ($match -eq $true) {
+        $deviceID = $Matches[0]
+        #Write-Host "DeviceID   : $($deviceID)" -ForegroundColor Green
+    }
+    return $deviceID
+}
+
+function PNPInstallByVenDevID {
+    param (
+        [switch] $Install,
+        [array] $UnknownDeviceList,
+        [object] $InfFiles
+    )
+    if ($Install -eq $false) {
+        Write-Host "=============================================================" -ForegroundColor Cyan
+        Write-Host "*** Running in TEST mode. Add -Install to install drivers ***" -ForegroundColor Cyan
+        Write-Host "=============================================================" -ForegroundColor Cyan
+    }
+    $UnknownDeviceList.DeviceID | ForEach-Object {
+        $_
+        if ($_ -eq $null) {
+            return
+        }
+        $vendorID = GetVendorID -string $_
+        $deviceID = GetDeviceID -string $_
+        Write-Host "VEN: $vendorID & DEV: $deviceID"
+        #$pattern = VEN_1B36&DEV_0100
+        $pattern = "VEN_$vendorID&DEV_$deviceID"
+        Write-Host "Pattern: $pattern"
+
+        foreach ($infFile in $InfFiles) {
+            Write-Host "[+] $infFile >>> $($infFile.FullName)" -ForegroundColor Cyan
+            $result = CheckDeviceIDInFile -FilePath $($infFile.FullName) -Pattern $pattern
+            if ($result -eq $true) {
+                Write-Host " - Found : $pattern" -ForegroundColor Green
+                if ($Install -eq $true) {
+                    Write-Host "*** Install driver: $($infFile.FullName)***" -ForegroundColor Green
+                    pnputil.exe /add-driver $($infFile.FullName) /install /force
+                    #$UnknownDeviceList = GetUnknownDevices  # New Line for refresh unknown device list after install driver
                 }
             } else {
                 Write-Host " - skip: $pattern" -ForegroundColor Gray
