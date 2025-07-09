2af8f0272049de9d850553c4df000563fe5112c5
Mrofyum <morfyum@gmail.com>
2025-07-09 20:44:13 +0100

    NEW finder.ps1
    NEW: symptoms.ps1

---
+++ src/tools/finder.ps1
@@ -0,0 +1,94 @@
+﻿function Search-FileContentAndName {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path,
+
+        [Parameter(Mandatory=$true)]
+        [string]$Keyword,
+
+        [switch]$SearchOnlyInMatchingNames = $false,
+        [string[]]$IncludeExtensions = @("*"),
+        [string[]]$ExcludeExtensions = @(".dat",".wim", ".ttf", ".exe", ".dll", ".zip", ".rar", ".7z", ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".mp3", ".mp4", ".avi", ".mkv", ".pdf", ".docx", ".xlsx", ".pptx", ".odt", ".ods", ".odp")
+    )
+
+    Write-Host "PowerShell finder..." -ForegroundColor Green
+    Write-Host "Path:    $Path" -ForegroundColor Cyan
+    Write-Host "Keyword: $Keyword" -ForegroundColor Cyan
+    Write-Host "Search only in files matching the name: $($SearchOnlyInMatchingNames)" -ForegroundColor Cyan
+    Write-Host "Included Extensions: $($IncludeExtensions -join ', ')" -ForegroundColor Cyan
+    Write-Host "Excluded Extensions: $($ExcludeExtensions -join ', ')" -ForegroundColor Cyan
+    Write-Host ""
+
+    $foundFilesByName = @()
+    $foundFilesByContent = @()
+
+    # Fájlnevek keresése
+    Write-Host "Searching for filenames with keyword '$Keyword'" -ForegroundColor Yellow
+    Get-ChildItem -Path $Path -Recurse -File -Include $IncludeExtensions -ErrorAction SilentlyContinue| ForEach-Object {
+        if ($_.Name -like "*$Keyword*") {
+            $foundFilesByName += $_.FullName
+            Write-Host "# Match by Name: $($_.FullName)" -ForegroundColor Green
+        }
+    }
+
+    Write-Host ""
+    Write-Host "Searching file contents using '$Keyword'..." -ForegroundColor Yellow
+
+    $filesToSearch = @()
+
+    if ($SearchOnlyInMatchingNames -and $foundFilesByName.Count -eq 0) {
+        Write-Host "No file with the keyword in its name. Skipping content search." -ForegroundColor Yellow
+    }
+    elseif ($SearchOnlyInMatchingNames) {
+        Write-Host "Search for content only in files found in the name..." -ForegroundColor Yellow
+        $filesToSearch = $foundFilesByName
+    }
+    else {
+        $filesToSearch = Get-ChildItem -Path $Path -Recurse -File -Include $IncludeExtensions -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+    }
+
+    foreach ($file in $filesToSearch) {
+        # Check excluded extensions
+        $fileExtension = [System.IO.Path]::GetExtension($file)
+        if ($fileExtension -ne "") {
+            if ($ExcludeExtensions -contains $fileExtension.ToLower()) {
+                Write-Host "SKIP: $($file)" -ForegroundColor DarkGray
+                continue
+            }
+        }
+
+        try {
+            Select-String -Path $file -Pattern $Keyword -CaseSensitive:$false -ErrorAction SilentlyContinue | ForEach-Object {
+                $line = $_.Line
+                $lineNumber = $_.LineNumber
+                $foundFilesByContent += [PSCustomObject]@{
+                    Path = $_.Path
+                    LineNumber = $lineNumber
+                    LineContent = $line
+                }
+                Write-Host "File: $($_.Path) (Line: $lineNumber): $line" -ForegroundColor Green
+            }
+        }
+        catch {
+            Write-Warning "An error occurred while reading file: $($file). Error: $($_.Exception.Message)"
+        }
+    }
+
+    Write-Host "--- Summary ---" -ForegroundColor Green
+    Write-Host "# Files matched in name ($($foundFilesByName.Count) pcs):" -ForegroundColor Green
+    if ($foundFilesByName.Count -gt 0) {
+        $foundFilesByName | ForEach-Object { Write-Host "  $_" }
+    } else {
+        Write-Host "Not found"
+    }
+
+    Write-Host "# Files matched in content ($($foundFilesByContent.Count) pcs):" -ForegroundColor Green
+    if ($foundFilesByContent.Count -gt 0) {
+        $foundFilesByContent | Format-Table -AutoSize
+    } else {
+        Write-Host "Not found"
+    }
+    Write-Host "Search Completed" -ForegroundColor Green
+}
+
+Search-FileContentAndName -Path C:\Windows\ -Keyword "malware"
---
+++ src/tools/symptoms/symptoms.ps1
@@ -0,0 +1,186 @@
+﻿<#  Windows symptoms By Morfyum
+    VERSION: 7
+    BORN: 2021-jan-18
+    COPYRIGHT: 2022 
+    WINDOWS VERSIONS: https://docs.microsoft.com/en-us/windows/release-information/
+#>
+
+$currentUser = "$env:UserName"
+$serialNumber = (Get-CimInstance win32_bios).SerialNumber
+$today = $((Get-Date).ToString('yyyy-MM-dd HHmm'))
+
+function EventLogFilter {
+    # DOCS: https://devblogs.microsoft.com/scripting/use-filterhashtable-to-filter-event-log-with-powershell/
+    # Leve: Information: [ 4 ] , Warning: [ 3 ], Error: [ 2 ], Critical: [ 1 ], LogAlways: [ 0 ].
+    param (
+        [string]$LogName,
+        [string]$LogID,
+        [string]$LogLevel,
+        [string]$LogCustomTitle,
+        [string]$LogCustomDescription
+    )
+
+    switch ($LogLevel) {
+        # 1:CRITICAL 2:ERROR 3:WARNING 4:INFORMATION
+        1 { $logLevelName = "CRITICAL" }
+        2 { $logLevelName = "ERROR" }
+        3 { $logLevelName = "WARNING" }
+        4 { $logLevelName = "INFORMATION" }
+    }
+
+    try {
+        #by source:  Get-EventLog -LogName System -EntryType Error, Warning -Source nvlddmkm -ErrorAction stop
+        $eventLogs = Get-WinEvent -FilterHashtable @{logname="$($LogName)"; ID=$LogID; level=$LogLevel} -ErrorAction stop
+        $eventLogsCounted = $eventLogs.Count
+        $eventLogName = $eventLogs[0].ProviderName
+        $eventLogMessage = $eventLogs[0].Message
+        Write-Host "- $LogLevelName ($($eventLogsCounted)x) [$LogName] / eventID: $LogID / $LogLevel : [$LogCustomTitle]" -ForegroundColor Yellow
+        Write-Host "  Name: $eventLogName"
+        Write-Host "  Message: $eventLogMessage"
+    }
+    catch {
+        Write-Host "- [ OK ] [$LogCustomTitle] : [$LogName] / $LogID / $LogLevel" -ForegroundColor Green
+    }
+}
+
+
+function GetBSODInfo {
+    # DOCS: https://devblogs.microsoft.com/scripting/use-filterhashtable-to-filter-event-log-with-powershell/
+    Write-Host "# Event Log Analyze: BSOD" -ForegroundColor Cyan    
+    $pattern = "0x.{8}"
+    $BSOD = $null
+    try {
+        $BSODLogs = Get-WinEvent -FilterHashtable @{LogName='System';Id=1001; level=2 } -ErrorAction stop
+        $BSODLogs | ForEach-Object {
+            Write-Host "- $($_.ProviderName)" -ForegroundColor Red
+            Write-Host "$($_.Message)"
+            if ($($_.Message) -match $pattern) {
+                $Matches.Values | ForEach-Object {
+                    $BSOD =$_
+                    Write-Host "BSOD: $BSOD"
+                }
+            } else {
+                Write-Host "Nincs találat."
+            }
+        }
+        #$BSODLogName = $BSODLogs[0].ProviderName
+        #$BSODLogMessage = $BSODLogs[0].Message
+        #Get-WinEvent -FilterHashtable @{logname='System'; ID=1001; level=2}
+        #$BSODLogs
+    }
+    catch {
+        Write-Host "- [ OK ] eventID: 1001 [ BSOD ]: not found." -ForegroundColor Green
+    }
+
+}
+
+<#
+$osRawVersion = [System.Environment]::OSVersion.Version | Select-Object Build | Select-String [0-9]
+$osClearVersion = $osRawVersion -replace "[^0-9]",""
+# 2004 > 1903 > old
+# 18362 = 1903
+# 18363 = 1909
+# 19041 = 2004
+# 19042 = 20H2
+# 22000 = 21H2 22000.258
+$checkNetFrameworkIsInstalled = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release -ge 378389
+$listNetFrameworkVersions = Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP' -recurse | Get-ItemProperty -name Version,Release -EA 0 | Where { $_.PSChildName -match '^(?!S)\p{L}'} | Select PSChildName, Version, Release
+Test-Path "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v3.0\*
+#>
+
+function CheckMinidumpFolder {
+    Write-Host "# MINIDUMP" -ForegroundColor Cyan
+    $bsodPath = "C:\Windows\minidump"
+    switch (Test-Path $bsodPath){
+        "True" {
+            Write-Host "- Minidump directory exists: [$bsodPath ]" -ForegroundColor Yellow
+        }
+        "False" {
+            Write-Host "- [ OK ] $bsodPath not found" -ForegroundColor Green
+        }
+    }
+}
+
+function CheckUnknownDevices {
+    Write-Host "# Check Unknown Devices" -ForegroundColor Cyan
+    $unknownDevices = Get-WmiObject Win32_PNPEntity | Where-Object {$_.ConfigManagerErrorCode -gt 0 }
+
+    if ($null -eq $unknownDevices){
+        Write-Host "[ OK ] DEVICE MANAGER (no unknown device)" -ForegroundColor Green
+    } else {
+        Write-Host "[ FAIL ] 1 OR MORE UNKNOWN DEVICE FOUND!" -ForegroundColor Red
+        $unknownDevices
+    }
+   
+}
+
+
+function StartAppCamera {
+    Write-Host "# START APP: CAMERA" -ForegroundColor Cyan
+
+    $startAppxList = @(
+        'Microsoft.WindowsCamera'
+        #'Microsoft.WindowsSoundRecorder'
+    )
+    foreach ($element in $startAppxList) {
+        Write-Host "- $element"
+        Start-Sleep -Seconds 2
+        Get-AppxPackage *$element* | % {& Explorer.exe $('Shell:AppsFolder\' + $_.PackageFamilyName + ‚!' + $((Get-AppxPackageManifest $_.PackageFullName).Package.Applications.Application.id))}
+    }
+}
+
+
+function GetWindowsLicensingStatus {
+    #Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" |
+    #where { $_.PartialProductKey } | select LicenseStatus
+    #slmgr /xpr
+    # Get-CimInstance SoftwareLicensingProduct -Filter "LicenseStatus = 1"
+    # Get-CimInstance SoftwareLicensingProduct -Filter "LicenseStatus = 1" | Select-Object Description, LicenseStatus, ProductKeyChannel, PartialProductKey
+    #$lStatus = (Get-CimInstance SoftwareLicensingProduct -Filter "Name like 'Windows%'" | where { $_.PartialProductKey } | select LicenseStatus).LicenseStatus
+    $lStatus = (Get-CimInstance -Query "SELECT LicenseStatus from SoftwareLicensingProduct WHERE name LIKE 'Windows%' AND LicenseStatus = 1").LicenseStatus
+    if ( $lStatus -eq 1 ){
+        Write-Host "- [ OK ] Windows has active License" -ForegroundColor Green;
+    }
+    elseif ( $lStatus -eq 0 ){
+        Write-Host "- [ Oops ] Windows Activation Required" -ForegroundColor Red;
+    }
+    else{
+        Write-Host "- [ Oops ] Something Went Wrong. Please report this issue" -ForegroundColor Yellow;
+    }
+}
+
+########################################################################################################################
+########################################################################################################################
+Write-Host "# POWER LOSS" -ForegroundColor Cyan
+EventLogFilter -LogName "System" -LogID 41 -LogLevel 1 -LogCustomTitle "CRITICAL Kernel-Power"
+Write-Host "# MB/WLAN / ?PCI" -ForegroundColor Cyan 
+EventLogFilter -LogName "System" -LogID 17 -LogLevel 1 -LogCustomTitle "CRITICAL WHEA-Logger"
+EventLogFilter -LogName "System" -LogID 17 -LogLevel 3 -LogCustomTitle "WARNING WHEA-Logger"
+Write-Host "# WLAN / ?PCI" -ForegroundColor Cyan
+#CRITICAL: RTWLanE 5002 - wifi problema
+EventLogFilter -LogName "System" -LogID 5001 -LogLevel 2 -LogCustomTitle "ERROR RTWlanE (slow wifi speed?)"
+EventLogFilter -LogName "System" -LogID 5002 -LogLevel 1 -LogCustomTitle "CRITICAL RTWLanE 5002 - wifi problema"
+EventLogFilter -LogName "System" -LogID 5002 -LogLevel 2 -LogCustomTitle "ERROR RTWLanE 5002 - wifi problema"
+EventLogFilter -LogName "System" -LogID 5006 -LogLevel 2 -LogCustomTitle "ERROR RTWlanE (slow wifi speed?)"
+EventLogFilter -LogName "System" -LogID 5010 -LogLevel 2 -LogCustomTitle "ERROR Netwtw10"
+Write-Host "# DISC/FILESYSTEM" -ForegroundColor Cyan
+EventLogFilter -LogName "System" -LogID 3 -LogLevel 2 -LogCustomTitle "FilterMaster"
+EventLogFilter -LogName "System" -LogID 7 -LogLevel 2 -LogCustomTitle "Disc"
+EventLogFilter -LogName "System" -LogID 51 -LogLevel 3 -LogCustomTitle "Disc"
+EventLogFilter -LogName "System" -LogID 55 -LogLevel 2 -LogCustomTitle "Disc"
+EventLogFilter -LogName "System" -LogID 98 -LogLevel 3 -LogCustomTitle "Ntfs (Microsoft-Windows-Ntfs) 98"
+EventLogFilter -LogName "System" -LogID 140 -LogLevel 3 -LogCustomTitle "Ntfs"
+EventLogFilter -LogName "System" -LogID 4102 -LogLevel 2 -LogCustomTitle "ERROR iaStorAC (SMART ERROR)"
+Write-Host " DISC PERFORMANCE" -ForegroundColor Cyan
+EventLogFilter -LogName "System" -LogID 153 -LogLevel 3 -LogCustomTitle "WARNING Disc I/O performance issue"
+Write-Host "# NVIDIA" -ForegroundColor Cyan
+EventLogFilter -LogName "System" -LogID 4101 -LogLevel 3 -LogCustomTitle "Nvidia/Display"
+EventLogFilter -LogName "System" -LogID 13 -LogLevel 2 -LogCustomTitle "Nvidia: nvlddmkm"
+
+GetBSODInfo
+CheckMinidumpFolder
+CheckUnknownDevices
+Write-Host "# Check Windows Licensing Status" -ForegroundColor Cyan
+GetWindowsLicensingStatus
+#StartAppCamera
+pause
